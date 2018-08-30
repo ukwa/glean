0ed683c166e069bdd53d6d32edcc98676e82e002
Andrew Jackson <Andrew.Jackson@bl.uk>
2018-08-30 12:57:36 +0100

    Switch to extracting a single landing-page item with multiple documents.

--- docuharv/docuharv/items.py
+++ docuharv/docuharv/items.py
@@ -8,7 +8,47 @@
 import scrapy
 
 
-class DocuharvItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class DocumentItem(scrapy.Item):
+    # Document Title
+    title = scrapy.Field()
+    # Original URL for the document:
+    document_url = scrapy.Field()
+    # Wayback timestamp of the document:
+    wayback_timestamp = scrapy.Field()
+    # The filename of the Document:
+    filename = scrapy.Field()
+    # The size of the Document in bytes:
+    size = scrapy.Field()
+    # The ISBN of this Document (if known):
+    isbn = scrapy.Field()
+    # The DOI of this Document (if known):
+    doi = scrapy.Field()
+    # Command or Act paper reference (if any, UK GOV only):
+    command_paper_number = scrapy.Field()
+    house_of_commons_paper_number = scrapy.Field()
+
+
+class LandingPageItem(scrapy.Item):
+    # Landing page title:
+    title = scrapy.Field()
+    # The Document's Landing Page:
+    landing_page_url = scrapy.Field()
+    # Wayback timestamp of the landing page:
+    wayback_timestamp = scrapy.Field()
+    # The most recent publication date of this Document:
+    publication_date = scrapy.Field()
+    # The first publication date of this Document (if known):
+    first_publication_date = scrapy.Field()
+    # An array of authors for the document:
+    authors = scrapy.Field()
+    # An array of publishers for this document:
+    publishers = scrapy.Field()
+
+    # The array of resource documents associated with this landing page:
+    documents = scrapy.Field()
+
+    # The original metadata payload from the publisher (if any, JSON encoded):
+    publisher_metadata = scrapy.Field()
+
+    # The ID of the Target this Document should be associated with (if any):
+    target_id = scrapy.Field()
--- docuharv/docuharv/spiders/gov_uk_pub.py
+++ docuharv/docuharv/spiders/gov_uk_pub.py
@@ -1,7 +1,8 @@
+import json
 import scrapy
 from scrapy.selector import HtmlXPathSelector
-import json
 from urllib.parse import urlsplit, urlunsplit
+from ..items import LandingPageItem, DocumentItem
 
 
 class GovUkSpider(scrapy.Spider):
@@ -29,11 +30,36 @@ class GovUkSpider(scrapy.Spider):
             yield response.follow(api_json_url, self.parse_content_api_json)
 
     def parse_content_api_json(self, response: scrapy.http.Response):
-        # Just return it all
+        # Build up an item
         md = json.loads(response.body)
+        item = LandingPageItem()
+        item['landing_page_url'] = response.url
+        item['title'] = md['title']
+        item['first_publication_date'] = md['first_published_at']
+        item['publication_date'] = md['first_published_at']
+        # Pick up the 'public updated' date instead, if present:
+        if 'public_updated_at' in md:
+            item['publication_date'] = md['public_updated_at']
+        item['publishers'] = []
+        for org in md['links']['organisations']:
+            item['publishers'].append(org['title'])
+        # item['publisher_metadata'] = md
+
+        # Make a response object to make it easy to parse the HTML fragments in the API:
         resp = response.copy()
+
+        # Go through the documents:
+        item['documents'] = []
         for doc in md['details']['documents']:
             resp._set_body(doc)
-            frag = HtmlXPathSelector(resp)
-            yield { 'title': resp.css('.title ::text').extract_first() }
+            doc_item = DocumentItem()
+            doc_item['title'] = resp.css('.title ::text').extract_first()
+            doc_item['document_url'] = response.urljoin(resp.css('.attachment-details a::attr(href)  ').extract_first())
+            doc_item['isbn'] = resp.css('span[class=isbn] ::text').extract_first()
+            doc_item['command_paper_number'] = resp.css('span[class=command_paper_number] ::text').extract_first()
+            doc_item['house_of_commons_paper_number'] = resp.css('span[class=house_of_commons_paper_number] ::text').extract_first()
+            item['documents'].append(dict(doc_item))
+
+        # Return the composite ite:
+        yield item
 
